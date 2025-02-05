6865ee741a38ffc11f746b15eb093b2cc5e61cb9
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-02-05 15:48:53 -0300

    chore: update index export

--- packages/core/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/tokens';
-export * from './src/styles';
-export * from './src/utils';
-export * from './src/types';
--- packages/react/index.ts
+++
@@ -1,2 +0,0 @@
-export { Button } from "./src/components/Button/Button";
-export { Input } from "./src/components/Input/Input";

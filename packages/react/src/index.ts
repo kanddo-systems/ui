799ce233473c302100be8decf9877e221718f7d0
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-02-06 21:15:57 -0300

    chore: update lib version

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,3 +2,4 @@ export { Button } from "./components/Button/Button";
 export { Input } from "./components/Input/Input";
 export { Typography } from "./components/Typography/Typography";
 export { Card } from "./components/Card/Card";
+export { Accordion } from "./components/Accordion/Accordion";

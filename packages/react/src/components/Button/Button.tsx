a303f3461c8143dc740bd4574d9521e61bfa8624
Marcelo dos Reis <marcelohrpaulo13@gmail.com>
2025-03-14 22:33:02 -0300

    refactor: button

--- packages/react/src/components/Button/Button.tsx
+++ packages/react/src/components/Button/Button.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { FC, memo } from 'react';
 import { RoundedSize } from '@kanddo-ui/core';
 import { Component } from './Button.styles';
 
 export interface ButtonProps {
-    children: React.ReactNode;
-    rounded?: RoundedSize;
-    onClick?: () => void;
-    disabled?: boolean;
+  children: React.ReactNode;
+  rounded?: RoundedSize;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-    children,
-    onClick,
-    rounded = 'medium',
-    disabled = false
+const ButtonComponent: FC<ButtonProps> = ({
+  children,
+  onClick,
+  rounded = 'medium',
+  disabled = false,
 }) => {
-    return (
-        <Component rounded={rounded} onClick={disabled ? undefined : onClick} disabled={disabled}>
-            {children}
-        </Component>
-    );
+  return (
+    <Component
+      rounded={rounded}
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
+      aria-disabled={disabled}
+      role="button"
+    >
+      {children}
+    </Component>
+  );
 };
+
+export const Button = memo(ButtonComponent);
